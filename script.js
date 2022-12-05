329ca75138d81d65e58e39b72d2efb7be3add43c
Pierre M <109650634+Gollumeo@users.noreply.github.com>
2022-12-05 09:58:05 +0100

    navigation done

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-/*
-*/
+const OL = document.querySelector("ol *:last-child");
+// const LAST_CHILD = OL.lastChild;
+
+console.log(OL);
+
+const MAIN = document.querySelector('main');
+const secondSection = MAIN.children[1];
+const thirdSection = MAIN.children[2];
+const secondTitle = secondSection.querySelector('h2');
+const thirdTitle = thirdSection.querySelector('h2');
+
+secondSection.appendChild(thirdTitle);
+thirdSection.appendChild(secondTitle);
+
+const secondSectionFirstChild = secondSection.children[0];
+secondSection.insertBefore(thirdTitle, secondSectionFirstChild);
+
+const thirdSectionFirstChild = thirdSection.children[0];
+thirdSection.insertBefore(secondTitle, thirdSectionFirstChild);
+
+MAIN.removeChild(thirdSection);
